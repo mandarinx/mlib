c1ac1c7b36f6366c5b448e2d76937694671dbe03
mandarinx <mandarin@mandarin.no>
2018-08-09 11:22:29 +0200

    Added method for drawing gizmo arcs

--- Assets/Utils/GizmoUtils.cs
+++ Assets/Utils/GizmoUtils.cs
@@ -1,27 +1,115 @@
 ﻿using UnityEngine;
 
-namespace mlib {
+namespace Mlib {
 
     public static class GizmoUtils {
 
-        public static void DrawCircle(Vector3 position, float radius, Color color) {
-            Gizmos.color = color;
+        public const float DRAW_RES = Mathf.PI * 2f / 32f;
+
+        public static void DrawCircleXY(Vector3 position, float radius) {
+            DrawCircle(position, radius, Vector3.forward);
+        }
+
+        public static void DrawCircleXZ(Vector3 position, float radius) {
+            DrawCircle(position, radius, Vector3.up);
+        }
+
+        public static void DrawCircleYZ(Vector3 position, float radius) {
+            DrawCircle(position, radius, Vector3.right);
+        }
+
+        public static void DrawArcXY(Vector3 position,
+                                     float   radius,
+                                     float   startRadians,
+                                     float   endRadians) {
+            DrawArc(position, radius, startRadians, endRadians, Vector3.forward);
+        }
+
+        public static void DrawArcXZ(Vector3 position,
+                                     float   radius,
+                                     float   startRadians,
+                                     float   endRadians) {
+            DrawArc(position, radius, startRadians, endRadians, Vector3.down);
+        }
+
+        public static void DrawArcYZ(Vector3 position,
+                                     float   radius,
+                                     float   startRadians,
+                                     float   endRadians) {
+            DrawArc(position, radius, startRadians, endRadians, Vector3.right);
+        }
+
+        public static void DrawCircle(Vector3 position, float radius, Vector3 normal) {
+            Vector3[] circleCoords;
+            int numCoords = GetCircleCoords(radius, DRAW_RES, out circleCoords);
+            if (numCoords == 0) {
+                return;
+            }
+
+            Vector3 lastPos = RotateVector(circleCoords[0], normal);
+            for (int i = 1; i < numCoords; ++i) {
+                Gizmos.DrawLine(position + lastPos,
+                                position + RotateVector(circleCoords[i], normal));
+                lastPos = RotateVector(circleCoords[i], normal);
+            }
+
+            Gizmos.DrawLine(position + RotateVector(circleCoords[circleCoords.Length - 1], normal),
+                            position + RotateVector(circleCoords[0], normal));
+        }
+
+        // res = angle resolution in radians
+        public static int GetCircleCoords(float radius, float res, out Vector3[] coords) {
             float theta = 0f;
             float x = radius * Mathf.Cos(theta);
             float y = radius * Mathf.Sin(theta);
-            Vector3 pos = position + new Vector3(x, y, 0f);
-            Vector3 lastPos = pos;
             const float TWOPI = Mathf.PI * 2;
-            const float inc = 0.4f;
-            for (theta = inc; theta < TWOPI; theta += inc) {
+            int segments = Mathf.FloorToInt(TWOPI / res);
+            coords = new Vector3[segments];
+            coords[0] = new Vector3(x, y, 0f);
+            int ci = 1;
+            for (theta = res; theta < TWOPI; theta += res, ++ci) {
                 x = radius * Mathf.Cos(theta);
                 y = radius * Mathf.Sin(theta);
-                Vector3 newPos = position + new Vector3(x, y, 0f);
-                Gizmos.DrawLine(pos, newPos);
-                pos = newPos;
+                if (ci < segments) {
+                    coords[ci] = new Vector3(x, y, 0f);
+                }
             }
 
-            Gizmos.DrawLine(pos, lastPos);
+            return segments;
+        }
+
+        public static Vector3 RotateVector(Vector3 vector, Vector3 normal) {
+            return Quaternion.LookRotation(normal, Vector3.up) * vector;
+        }
+
+        public static void DrawArc(Vector3 pos,
+                                   float   radius,
+                                   float   startRadians,
+                                   float   endRadians,
+                                   Vector3 normal) {
+
+            if (startRadians > endRadians) {
+                float start = startRadians;
+                startRadians = endRadians;
+                endRadians = start;
+            }
+
+            startRadians = Mathf.Clamp(startRadians, 0f, Mathf.PI * 2f);
+            endRadians = Mathf.Clamp(endRadians, 0f, Mathf.PI * 2f);
+            float diff = endRadians - startRadians;
+            int segments = Mathf.CeilToInt(diff / DRAW_RES);
+            Vector3 from = new Vector3(Mathf.Cos(startRadians) * radius,
+                                       Mathf.Sin(startRadians) * radius);
+            from = RotateVector(from, normal);
+
+            for (int i = 1; i <= segments; ++i) {
+                float rad = Mathf.Clamp(startRadians + DRAW_RES * i, startRadians, endRadians);
+                Vector3 to = new Vector3(Mathf.Cos(rad) * radius,
+                                         Mathf.Sin(rad) * radius);
+                to = RotateVector(to, normal);
+                Gizmos.DrawLine(pos + from, pos + to);
+                from = to;
+            }
         }
     }
 }
